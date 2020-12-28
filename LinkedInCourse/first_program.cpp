14e893e232d5cbe03ad88ae2b1f6415d9aaa6034
febin-varghese <febinvarghese994@gmail.com>
2020-12-28 13:07:08 +0100

    First commit for checking

---
+++ LinkedInCourse/first_program.cpp
@@ -0,0 +1,15 @@
+/*
+Hello World Program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter your name" << endl;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CppProjects
+Practice problems to learn and master C++
+
+## LinkedInCourse
+This folder contains the problems in the course "Advance Your Skills in C++" in LinkedIn
